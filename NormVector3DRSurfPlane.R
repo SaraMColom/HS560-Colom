563f23ffafb2dace93ac1f4cf58caca1edec5e62
SaraMColom <scolom@umich.edu>
2018-10-21 13:16:45 -0400

    Update NormVector3DRSurfPlane.R

--- NormVector3DRSurfPlane.R
+++ NormVector3DRSurfPlane.R
@@ -14,3 +14,23 @@ list("NormVec"=normVec,"dmean"=dMean,"d"=d)
 }
 
 Results<-NormVec_dmean(Matrix=pca1,x=MyData$x,y=MyData$y,z=MyData$z)
+
+# Create regression plane for bivariate linear model
+###   Define 3d features
+
+BivariatePlane<-function(data,x,y,z){
+fit<-lm(z~x+y,data) 
+coef.lm.fit<-coef(fit)
+
+x.seq = seq(min(x), max(x),length.out = 100)
+y.seq = seq(min(y), max(y),length.out = 100)
+z.seq <- function(x,y) coef.lm.fit[1]+coef.lm.fit[2]*x+coef.lm.fit[3]*y
+
+# define the values of z = z(x.seq, y.seq), as a Matrix of 
+# dimension c(dim(x.seq), dim(y.seq))
+z <- t(outer(x.seq, y.seq, z.seq))
+residuals<-fit$residuals  
+  
+list("x.seq"=x.seq,"y.seq"=y.seq,z=z,residuals=residuals)
+}
+
